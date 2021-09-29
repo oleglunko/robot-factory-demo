61a68238840baa5ccfd1fc417084416854cda59a
Oleg Lunko <oleglunko@gmail.com>
2021-09-29 12:25:13 +0700

    Implement scientist

--- src/ru/oleglunko/robotfactory/entity/Scientist.java
+++ src/ru/oleglunko/robotfactory/entity/Scientist.java
@@ -1,17 +1,35 @@
 package ru.oleglunko.robotfactory.entity;
 
 
-import java.util.List;
+import java.util.*;
 
 public class Scientist {
 
-    private String name;
+    private final Map<RobotDetail, Integer> robotDetails = new HashMap<>();
+    private final String name;
 
     public Scientist(String name) {
         this.name = name;
     }
 
-    public void takeRobotDetails(List<RobotDetail> takenDetails) {
+    public void takeRobotDetails(List<RobotDetail> list) {
+        list.forEach(detail -> robotDetails.merge(detail, 1, Integer::sum));
+    }
+
+    public List<Robot> buildRobot() {
+        List<Robot> robots = new LinkedList<>();
+        if (isEnoughPartsToBuild()) {
+            int robotAmount = robotDetails.values().stream().min(Integer::compareTo).orElse(0);
+            for (int i = 0; i < robotAmount; i++) {
+                robots.add(new Robot());
+            }
+        }
+
+        return robots;
+    }
+
+    private boolean isEnoughPartsToBuild() {
+        return robotDetails.keySet().containsAll(RobotDetail.VALUES);
     }
 
     public String getName() {
